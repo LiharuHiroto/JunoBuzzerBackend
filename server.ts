b65d4df037ff501b2b9f29ba2c960012d5eda0e7
DharshanPalani <dharshanpalani.2006@gmail.com>
2026-02-23 13:04:15 +0530

    backend check

--- server.ts
+++ server.ts
@@ -116,5 +116,9 @@ io.on("connection", (socket: Socket) => {
   });
 });
 
+app.get("/", (req, res) => {
+  res.status(201).send("Hello from backend");
+});
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
